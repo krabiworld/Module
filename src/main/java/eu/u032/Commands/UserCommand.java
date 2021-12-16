4922ded8d9a9ddceac2bb53a156626b148b1e140
HeadcrabJ <94139390+HeadcrabJ@users.noreply.github.com>
2021-12-16 11:57:50 +0500

    added user command

---
+++ src/main/java/eu/u032/Commands/UserCommand.java
@@ -0,0 +1,43 @@
+package eu.u032.Commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
+
+import java.awt.*;
+
+public class UserCommand extends Command {
+
+    public UserCommand() {
+        this.name = "user";
+        this.help = "Information about user";
+        this.category = new Category("Information");
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        try {
+            Member member =
+                    event.getArgs().isEmpty() ? event.getMember() : event.getGuild().retrieveMemberById(event.getArgs()).complete();
+
+            String value = String.format("**Username:** %s\n**ID:** %s\n**Color:** #%06x\n**Joined at:** <t:%s>\n**Registered at:** <t:%s>",
+                    member.getUser().getAsTag(),
+                    member.getId(),
+                    member.getColor().getRGB() & 0xFFFFFF,
+                    member.getTimeJoined().toEpochSecond(),
+                    member.getTimeCreated().toEpochSecond()
+            );
+
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setAuthor("Information about " + member.getUser().getName(), member.getEffectiveAvatarUrl())
+                    .setColor(Color.decode("#6196d5"))
+                    .setDescription(value)
+                    .setThumbnail(member.getEffectiveAvatarUrl());
+            event.reply(embed.build());
+        } catch (Exception e) {
+            event.replyError(e.getMessage());
+        }
+    }
+}
