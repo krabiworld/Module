c8d3657526500c90f6bc034e706c4f1de6aadcb3
krabiworld <krabiworld@proton.me>
2021-12-18 14:49:10 +0500

    small fixes

--- src/main/java/eu/u032/Commands/KickCommand.java
+++ src/main/java/eu/u032/Commands/KickCommand.java
@@ -2,34 +2,32 @@ package eu.u032.Commands;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
-import eu.u032.Utils.Config;
 import net.dv8tion.jda.api.Permission;
 
-public class MuteCommand extends Command {
+public class KickCommand extends Command {
 
-    public MuteCommand() {
-        this.name = "mute";
+    public KickCommand() {
+        this.name = "kick";
         this.help = "Mute member";
-        this.userPermissions = new Permission[]{Permission.MANAGE_ROLES};
+        this.userPermissions = new Permission[]{Permission.KICK_MEMBERS};
         this.category = new Category("Moderation");
     }
 
     @Override
     protected void execute(CommandEvent event) {
-        if (event.getArgs().isEmpty()) {
+        String[] args = event.getArgs().split("\\s+");
+
+        if (args[0].isEmpty()) {
             event.replyError("Required arguments are missing!");
             return;
         }
 
         try {
-            event.getGuild().addRoleToMember(
-                    event.getArgs(), event.getJDA().getRoleById(Config.getString("MUTE_ROLE"))
-            ).queue();
-
+            event.getGuild().retrieveMemberById(args[0]).complete()
+                    .kick(args[1]).queue();
             event.reactSuccess();
         } catch (Exception e) {
             event.replyError(e.getMessage());
         }
     }
-
 }
