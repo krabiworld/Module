146eb86f5006409a55b6139345711bbfc88eb17c
HeadcrabJ <94139390+HeadcrabJ@users.noreply.github.com>
2021-12-19 16:03:35 +0500

    added stats command

---
+++ src/main/java/eu/u032/Commands/StatsCommand.java
@@ -0,0 +1,46 @@
+package eu.u032.Commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+
+import java.awt.*;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
+public class StatsCommand extends Command {
+
+    public StatsCommand() {
+        this.name = "stats";
+        this.help = "Get stats";
+        this.category = new Category("Information");
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        JDA jda = event.getJDA();
+        RuntimeMXBean mxBean = ManagementFactory.getRuntimeMXBean();
+        Runtime runtime = Runtime.getRuntime();
+
+        String common = String.format("**Servers:** %s\n**Users:** %s",
+                jda.getGuilds().size(), jda.getUsers().size()
+        );
+
+        String platform = String.format("**OS:** %s\n**Architecture:** %s\n**RAM Usage:** %sMB / %sMB\n**Ping:** %s ms\n**Uptime:** %s m",
+                System.getProperty("os.name"),
+                System.getProperty("os.arch"),
+                runtime.freeMemory() / 1024 / 1024, runtime.totalMemory() / 1024 / 1024,
+                jda.getGatewayPing(),
+                (mxBean.getUptime() / 1000) / 60
+        );
+
+        EmbedBuilder embed = new EmbedBuilder()
+                .setTitle("UASM Statistics")
+                .setColor(Color.decode("#6196d5"))
+                .addField("Common", common, true)
+                .addField("Platform", platform, true)
+                .setFooter("Java: " + System.getProperty("java.version"));
+        event.reply(embed.build());
+    }
+}
