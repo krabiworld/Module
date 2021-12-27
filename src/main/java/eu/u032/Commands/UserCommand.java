5b31ba8b058913d4b5f533d8d8e3f02d20360af1
krabiworld <krabiworld@proton.me>
2021-12-28 02:27:09 +0500

    Updated UserCommand.java

--- src/main/java/eu/u032/Commands/UserCommand.java
+++ src/main/java/eu/u032/Commands/UserCommand.java
@@ -6,6 +6,7 @@ import eu.u032.Utils;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.ClientType;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
 
 import java.util.Objects;
 
@@ -21,30 +22,32 @@ public class UserCommand extends Command {
     @Override
     protected void execute(CommandEvent event) {
         Member member = Utils.getMemberFromArgs(event) != null ? Utils.getMemberFromArgs(event) : event.getMember();
-        String status = ":black_circle: Offline";
-        String device = "Unknown";
+        String status;
+        StringBuilder device = new StringBuilder();
+        User.Profile profile = member.getUser().retrieveProfile().complete();
 
         status = switch (member.getOnlineStatus()) {
-            case ONLINE -> ":green_circle: Online";
-            case IDLE -> ":yellow_circle: Idle";
-            case DO_NOT_DISTURB -> ":red_circle: Do Not Disturb";
-            default -> status;
+            case ONLINE -> "<:online:925113750598598736>Online";
+            case IDLE -> "<:idle:925113750254682133>Idle";
+            case DO_NOT_DISTURB -> "<:dnd:925113750896398406>Do Not Disturb";
+            default -> "<:offline:925113750581817354>Offline";
         };
 
         for (ClientType type : member.getActiveClients()) {
             if (member.getUser().isBot()) {
-                device = ":cloud: Server";
+                device.append(":cloud: Server");
                 break;
             }
-            device = switch (type) {
-                case DESKTOP -> ":desktop: Desktop";
-                case MOBILE -> ":mobile_phone: Mobile";
-                case WEB -> ":spider_web: Web";
-                default -> device;
-            };
+            switch (type) {
+                case DESKTOP -> device.append(":desktop: Desktop");
+                case MOBILE -> device.append(":mobile_phone: Mobile");
+                case WEB -> device.append(":spider_web: Web");
+                case UNKNOWN -> device.append("Unknown");
+            }
+            device.append(" ");
         }
 
-        String description = String.format("**Username:** %s\n**Status:** %s\n**Device:** %s\n**Color:** #%s\n**Joined at:** <t:%s>\n**Registered at:** <t:%s>",
+        String description = String.format("**Username:** %s\n**Status:** %s\n**Device:** %s\n**Role color:** #%s\n**Joined at:** <t:%s>\n**Registered at:** <t:%s>",
                 member.getUser().getAsTag(),
                 status, device,
                 Integer.toHexString(Objects.requireNonNull(member.getColor()).getRGB()).substring(2),
@@ -54,10 +57,13 @@ public class UserCommand extends Command {
 
         EmbedBuilder embed = new EmbedBuilder()
                 .setAuthor("Information about " + member.getUser().getName())
-                .setColor(member.getColor())
+                .setColor(member.getColorRaw())
                 .setDescription(description)
                 .setThumbnail(member.getEffectiveAvatarUrl())
                 .setFooter("User ID: " + member.getId());
+
+        if (profile.getBannerUrl() != null) embed.setImage(profile.getBannerUrl() + "?size=512");
+
         event.reply(embed.build());
     }
 
