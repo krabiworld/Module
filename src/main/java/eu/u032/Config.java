fedfb6585947985f1a07c1215782698d931a4b87
krabiworld <krabiworld@proton.me>
2022-01-08 21:50:38 +0500

    added new line after license

--- src/main/java/eu/u032/Config.java
+++ src/main/java/eu/u032/Config.java
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with UASM. If not, see https://www.gnu.org/licenses/.
  */
+
 package eu.u032;
 
 import io.github.cdimascio.dotenv.Dotenv;
